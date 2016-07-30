7b65af0930b97e453f9c55703ab8c7ae8c5fe5a0
James Xue <jamesxue100@gmail.com>
2016-07-30 03:17:29 -0700

    with cat facts since ron swanson quotes API was deprecated

--- app.py
+++ app.py
@@ -5,14 +5,15 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True 
 
-url = "http://ron-swanson-quotes.herokuapp.com/quotes"
+url = "http://catfacts-api.appspot.com/api/facts?number=1"
 
 @app.route("/", methods=["GET", "POST"])
 def hello():
  
+    print requests.get(url).json()
     response_dict = requests.get(url).json()
     print response_dict
-    quote = response_dict["quote"]
+    quote = response_dict["facts"][0]
     num = random.randrange(0, 10)
     return_dict = [quote, num]
     print num
